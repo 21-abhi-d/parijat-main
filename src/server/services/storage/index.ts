14cea724b75b52e817f6769e0ea5c8eb065ff563
Name <name@example.com>
2026-04-05 19:43:44 +1000

    Delete everything

--- src/server/services/storage/index.ts
+++
@@ -1,8 +0,0 @@
-import type { IStorageService } from "./storage.interface";
-import { S3StorageService } from "./s3-storage.service";
-
-export function createStorageService(): IStorageService {
-  return new S3StorageService();
-}
-
-export type { IStorageService, PresignedUploadResult } from "./storage.interface";
